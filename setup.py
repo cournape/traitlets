6fd6ae94425f09765549fe89a3782dad9b8cb168
David Cournapeau <cournape@gmail.com>
2013-08-01 10:07:18 +0100

    ENH: add proper license info in the repo.

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from distutils.core import setup
 from traitlets import __version__
 
 if __name__ == "__main__":
-    setup(name="okonomiyaki",
+    setup(name="traitlets",
           author="David Cournapeau",
           author_email="David Cournapeau",
+          maintainer="David Cournapeau",
+          maintainer_email="David Cournapeau",
           packages=["traitlets",
                     "traitlets.tests",
                     ],
-          version=__version__
+          version=__version__,
+          licent="BSD",
     )
