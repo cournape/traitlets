4cbbb56bd6cf01bd3fe88357c73c4dab94187554
David Cournapeau <cournape@gmail.com>
2013-10-10 19:07:59 +0100

    BUG: 'export' Bool/CBool in top namespace.

--- traitlets/__init__.py
+++ traitlets/__init__.py
@@ -6,4 +6,4 @@ except ImportError:
 from ._implementation import HasTraits, MetaHasTraits, TraitType, Any, CBytes, Dict, \
     Int, Long, Integer, Float, Complex, Bytes, Unicode, TraitError, \
     Undefined, Type, This, Instance, TCPAddress, List, Tuple, \
-    ObjectName, DottedObjectName, CRegExp, Enum
+    ObjectName, DottedObjectName, CRegExp, Enum, Bool, CBool
